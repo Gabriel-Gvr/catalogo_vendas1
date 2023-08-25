e52ded59d2806524d91abff8eb9fcf5f21a4be99
Gabriel-Gvr <gabrielvrosa2@gmail.com>
2023-08-25 17:20:11 -0300

    att 25/08 finalizada

--- scripts/script.js
+++ scripts/script.js
@@ -1,39 +1,44 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const searchInput = document.querySelector(".search-input");
-    const searchButton = document.querySelector(".search-button");
-    const resetButton = document.querySelector(".reset-button");
-    const cardItems = document.querySelectorAll(".card-item");
+  const searchInput = document.querySelector(".search-input");
+  const searchButton = document.querySelector(".search-button");
+  const resetButton = document.querySelector(".reset-button");
+  const cardItems = document.querySelectorAll(".card-item");
 
-    // Função para mostrar todos os itens novamente
-    function showAllItems() {
-      cardItems.forEach((cardItem) => {
-        cardItem.style.display = "block";
-      });
-    }
-
-    // Event listener para o botão de pesquisa
-    searchButton.addEventListener("click", function () {
-      const searchTerm = searchInput.value.toLowerCase();
-
-      cardItems.forEach((cardItem) => {
-        const cardTitle = cardItem.querySelector("h3").textContent.toLowerCase();
-        const cardText = cardItem.querySelector("p").textContent.toLowerCase();
-
-        if (cardTitle.includes(searchTerm) || cardText.includes(searchTerm)) {
-          cardItem.style.display = "block";
-        } else {
-          cardItem.style.display = "none";
-        }
-      });
+  // Função para mostrar todos os itens novamente
+  function showAllItems() {
+    cardItems.forEach((cardItem) => {
+      cardItem.style.display = "block";
     });
+  }
 
- 
+  // Event listener para o botão de pesquisa
+  searchButton.addEventListener("click", function () {
+    const searchTerm = searchInput.value.toLowerCase();
 
-    // Event listener para o botão de redefinição
-    resetButton.addEventListener("click", function () {
-      searchInput.value = ""; // Limpa o campo de pesquisa
-      showAllItems(); // Mostra todos os itens novamente
+    cardItems.forEach((cardItem) => {
+      const cardTitle = cardItem.querySelector("h3").textContent.toLowerCase();
+      const cardText = cardItem.querySelector("p").textContent.toLowerCase();
 
-    
+      if (cardTitle.includes(searchTerm) || cardText.includes(searchTerm)) {
+        cardItem.style.display = "block";
+      } else {
+        cardItem.style.display = "none";
+      }
     });
   });
+
+
+
+  // Event listener para o botão de redefinição
+  resetButton.addEventListener("click", function () {
+    searchInput.value = ""; // Limpa o campo de pesquisa
+    showAllItems(); // Mostra todos os itens novamente
+
+
+
+  });
+
+
+
+});
+
